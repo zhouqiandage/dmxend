2833e879080ad6523ee6ca5be7e909bf8af4d451
zhouqiandage <1477342208@qq.com>
2019-12-16 09:25:06 +0800

    asd

--- src/main/java/cn/dmx/App.java
+++ src/main/java/cn/dmx/App.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("cn.dmx.mapper")
 public class App {
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
--- src/main/java/cn/dmx/entity/DataType.java
+++ src/main/java/cn/dmx/entity/DataType.java
@@ -8,11 +8,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class DataType {
-      private Integer did;  //类别字典id
-      private String typeCode; //类型编码
-      private String typeName; //类型名称
-      private Integer valueId; //值id
-      private String valueName; //类型值名称
+      private Integer did;   //类别字典id
+      private String typeCode;  //类型编码
+      private String typeName;  //类型名称
+      private Integer valueId;  //值id
+      private String valueName;  //类型值名称
 
 
 }
